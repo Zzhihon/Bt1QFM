e83b1d8208aac78806c6ec1a212f31e5a794a89e
Zzhihon <zhihong114514@gmail.com>
2025-06-08 20:08:51 +0800

    修改env-config的加载机制

--- web/ui/public/config/poll-config.js
+++ web/ui/public/config/poll-config.js
@@ -2,7 +2,7 @@
   let currentText = '';
   async function check(){
     try {
-      const res = await fetch('/config/env-config.js?cache=' + Date.now());
+      const res = await fetch('/config/env-config.js');
       const text = await res.text();
       if(!currentText){
         currentText = text;
